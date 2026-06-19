e27c48d02797e7874a74e8478530d8ec3ecbeab2
kenzowatanabee <kenzo_watanabe@colpal.com>
2026-06-19 10:23:52 -0300

    feat: added new parse argument, specific ean search

--- main.py
+++ main.py
@@ -11,15 +11,12 @@ class CLIscraper:
         """
         Handles CLI input parsing configurations.
         """
-        
-        parser = argparse.ArgumentParser(
-            description="EAN Web Scraper"
-        )
+        parser = argparse.ArgumentParser(description="EAN Web Scraper")
         
         parser.add_argument(
             "target",
             choices=["amazon"],
-            help="E-commerce plataform you want to target."
+            help="E-commerce platform you want to target."
         )
         
         parser.add_argument(
@@ -36,29 +33,44 @@ class CLIscraper:
             help="Maximum number of concurrent network connections (Default: 5)."
         )
         
+        parser.add_argument(
+            "-e", "--ean",
+            type=str,
+            help="Pass a specific single EAN to search instead of loading a CSV file."
+        )
+        
         return parser.parse_args()
         
     async def run(self) -> None:
         args = self.parse_arguments()
-        
         os.makedirs(IMAGES_DIR, exist_ok=True)
         
-        print(f"Loading input source: {args.input}")
+        # determine EAN target input type
+        if args.ean:
+            print(f"🎯 Single EAN Mode initialized. Targeting: [{args.ean}]")
+            # wrap the single string in a list so the engine gets the array it expects
+            ean_list = [args.ean.strip()]
+        else:
+            print(f"📋 File Mode initialized. Loading input source: {args.input}")
+            try:
+                ean_list = FileHandler.load_eans_from_csv(args.input)
+            except Exception as e:
+                print(f"Critical Error reading input source: {e}")
+                return
         
-        try:
-            ean_list: List[str] = FileHandler.load_eans_from_csv(args.input)
-        except Exception as e:
-            print("Critical Error reading input source: {e}")
-            return
-        
+        # validation guard clause
         if not ean_list:
-            print("EAN List is empty. Task aborted.")
+            print("❌ EAN List/Target is empty. Task aborted.")
             return  
         
+        # fire up the Engine
         if args.target == "amazon":
             from targets.amazon.worker import AmazonScraper
             
-            engine = AmazonScraper(ean_list=ean_list, max_concurrent=args.concurrency)
+            # since args.concurrency defaults to 5, we can override it to 1 for a single item
+            concurrency_limit = 1 if args.ean else args.concurrency
+            
+            engine = AmazonScraper(ean_list=ean_list, max_concurrent=concurrency_limit)
             scraped_data = await engine.main()
             
             output_path = "data/output/amazon_results.json"
